4b6eab102df945d3e1c3788b928dfe6b91dd4049
Olakareem <olakareemomobolarinwa@gmail.com>
2024-09-02 17:40:41 +0100

    chat

--- src/app/api/services/route.js
+++ src/app/api/services/route.js
@@ -29,7 +29,7 @@ export async function POST(request) {
       subject: `Website activity from ${name}`,
       html: `
                 <p>Dear ${name},</p>
-                <p>We are excited to share that your free report, <strong>Antihypertensive White paper</strong>, is now available for you to download! We hope you find it valuable and insightful as you explore [the topic of the report].</p>
+                <p>We are excited to share that your free report: <strong>Winning in Anti-Malaria Segment through Innovation</strong>, is now available for you to download! We hope you find it valuable and insightful as you explore [the topic of the report].</p>
                 <p>But that’s not all! As a token of our appreciation for your continued interest, we are pleased to offer you an exclusive 30% discount on your next purchase of any paid report from our collection.</p>
                 <p style="background-color: yellow; padding: 2px;"><strong>Your Discount Code:</strong> PBR Life Sciences</p>
                 <p>You can use this code at checkout to save 30% on any of our premium reports. This is a limited-time offer, so be sure to take advantage of this opportunity to access even more in-depth insights and data.</p>
