6629b0fc7ff22179fd1925aa2f3b7fa602ad4745
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-03 12:40:14 +0100

    fixes

--- src/app/components/Modal.jsx
+++ src/app/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ isOpen, onClose, message }) => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full flex flex-col justify-center items-center ">
-            <p className='text-center'>{message} <br/> <span className='text-xs'>Please check your spam or junk folder if the email does not appear in your inbox.</span></p>
+            <p className='text-center'>Congratulations. Your Report is on the way. <br/> <span className='text-xs'>Please check your spam or junk folder if the email does not appear in your inbox.</span></p>
 
 
 
