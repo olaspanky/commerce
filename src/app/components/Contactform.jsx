ac82e765cd653e4e7722959e32358dc56869981f
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-18 10:50:53 +0100

    fixes

--- src/app/components/Contactform.jsx
+++ src/app/components/Contactform.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function ZohoCRMLeadForm() {
   const [formData, setFormData] = useState({
