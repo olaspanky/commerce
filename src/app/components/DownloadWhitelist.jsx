b0676f6cb122a879db45204030f37fd7e71f30db
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-20 00:35:37 +0100

    fix

--- src/app/components/DownloadWhitelist.jsx
+++ src/app/components/DownloadWhitelist.jsx
@@ -158,7 +158,6 @@
     };
   return (
     <div className=" mx-auto p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-6">PBR MIR Lead Generation</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
           <label 
